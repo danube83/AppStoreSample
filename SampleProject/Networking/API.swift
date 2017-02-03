a1265ece9bfc8b8d0d9a765cd2694f56dc7a8df3
이선범 <danube83@tmon.co.kr>
2017-02-03 22:29:30 +0900

    API 에 Mappble  제네릭 추

--- SampleProject/Networking/API.swift
+++ SampleProject/Networking/API.swift
@@ -19,16 +19,16 @@ fileprivate struct Host {
 }
 
 
-struct API {
+struct API<Model: Mappable> {
     let path: String
     let query: [String: Any]?
-    let arrayParse: ((Any) -> [Mappable]?)?
-    let objectParse: ((Any) -> Mappable?)?
+    let arrayParse: ((Any) -> [Model]?)?
+    let objectParse: ((Any) -> Model?)?
     var request: DataRequest {
         return Alamofire.request(Host.apiURL(by: path), parameters: query)
     }
     
-    init(path: String, query: [String: Any]? = nil, arrayParse: ((Any) -> [Mappable]?)? = nil, objectParse: ((Any) -> Mappable?)? = nil) {
+    init(path: String, query: [String: Any]? = nil, arrayParse: ((Any) -> [Model]?)? = nil, objectParse: ((Any) -> Model?)? = nil) {
         self.path = path
         self.query = query
         self.arrayParse = arrayParse
@@ -37,7 +37,7 @@ struct API {
 }
 
 
-func send<T: Mappable>(api: API, keyPath: String? = nil, arrayCallback: @escaping ([T]?)->()) {
+func send<T: Mappable>(api: API<T>, keyPath: String? = nil, arrayCallback: @escaping ([T]?)->()) {
     api.request.responseArray(keyPath: keyPath) { (response: DataResponse<[T]>) in
         guard let unwrappedResponse = response.result.value else {
             arrayCallback(nil)
@@ -45,13 +45,13 @@ func send<T: Mappable>(api: API, keyPath: String? = nil, arrayCallback: @escapin
         }
         if let parse = api.arrayParse {
             let parsedResponse = parse(unwrappedResponse)
-            arrayCallback(parsedResponse as? [T])
+            arrayCallback(parsedResponse)
         }
         
     }
 }
 
-func send<T: Mappable>(api: API, keyPath: String? = nil, objectCallback: @escaping (T?)->()) {
+func send<T: Mappable>(api: API<T>, keyPath: String? = nil, objectCallback: @escaping (T?)->()) {
     api.request.responseArray(keyPath: keyPath) { (response: DataResponse<[T]>) in
         guard let unwrappedResponse = response.result.value else {
             objectCallback(nil)
@@ -59,16 +59,16 @@ func send<T: Mappable>(api: API, keyPath: String? = nil, objectCallback: @escapi
         }
         if let parse = api.objectParse {
             let parsedResponse = parse(unwrappedResponse)
-            objectCallback(parsedResponse as? T)
+            objectCallback(parsedResponse)
         }
         
     }
 }
 
 
-func topFreeApplicationAPI() -> API {
+func topFreeApplicationAPI() -> API<TopFreeApplicationModel> {
     let path = String(format: ApiPaths.topFreeApplications, arguments: [/*NSLocale.current.languageCode ??*/ "kr", 6015])
-    return API(path: path, arrayParse: {(response) -> [Mappable]? in
+    return API(path: path, arrayParse: {(response) -> [TopFreeApplicationModel]? in
         
         guard let convertedResponse = response as? [TopFreeApplicationModel] else {
             return nil
@@ -78,8 +78,8 @@ func topFreeApplicationAPI() -> API {
     })
 }
 
-func applicationDetailAPI(with identifier: String) -> API {
-    return API(path: ApiPaths.applicationDetail, query: ["id": identifier, "country": /*NSLocale.current.languageCode ??*/ "kr"]) { (response) -> Mappable? in
+func applicationDetailAPI(with identifier: String) -> API<ApplicationDetailModel> {
+    return API(path: ApiPaths.applicationDetail, query: ["id": identifier, "country": /*NSLocale.current.languageCode ??*/ "kr"]) { (response) -> ApplicationDetailModel? in
         
         guard let convertedResponse = response as? [ApplicationDetailModel] else {
             return nil;
